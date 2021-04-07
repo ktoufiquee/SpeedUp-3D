872d8cfaa23437ea13c3f4b0dd82e819a4e9fc68
ktoufiquee <59536321+ktoufiquee@users.noreply.github.com>
2021-04-07 23:18:03 +0600

    Base

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 camOffset { get; set; }
+
+    private void Start()
+    {
+        camOffset = transform.position;
+    }
+
+    private void Update()
+    {
+        transform.position = player.transform.position + camOffset;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float turnSpeed = 5.0f;
+    private float horizontalInput { get; set; }
+    private float verticalInput { get; set; }
+
+    private void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * (Time.deltaTime * speed * verticalInput) );
+        transform.Rotate(Vector3.up, (Time.deltaTime * turnSpeed * horizontalInput) );
+    }
+}
