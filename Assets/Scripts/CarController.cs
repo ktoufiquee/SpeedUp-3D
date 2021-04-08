e9644baa3fecf61c56ce4e4cdb6a15f0ca00bf2d
ktoufiquee <59536321+ktoufiquee@users.noreply.github.com>
2021-04-08 21:04:56 +0600

    Wheel Physics Added

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public Transform centerOfMass;
+    public float motorTorque = 100f;
+    public float maxSteer = 20f;
+
+    private float Steer { get; set; }
+    private float Throttle { get; set; }
+    
+    private Rigidbody _rigidbody;
+    private WheelController[] _wheels;
+    private void Start()
+    {
+        _wheels = GetComponentsInChildren<WheelController>();
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.centerOfMass = centerOfMass.localPosition;
+    }
+
+    private void Update()
+    {
+        Steer = Input.GetAxis("Horizontal");
+        Throttle = Input.GetAxis("Vertical");
+
+        foreach (var wheel in _wheels)
+        {
+            wheel.SteerAngle = Steer * maxSteer;
+            wheel.Torque = Throttle * motorTorque;
+        }
+
+    }
+}
---
+++ Assets/Scripts/WheelController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelController : MonoBehaviour
+{
+    public bool steer;
+    public bool invertSteer;
+    public bool power;
+
+    public float SteerAngle { get; set; }
+    public float Torque { get; set; }
+
+    private WheelCollider _wheelCollider;
+    private Transform _wheelTransform;
+    
+    private void Start()
+    {
+        _wheelCollider = GetComponentInChildren<WheelCollider>();
+        _wheelTransform = GetComponentInChildren<MeshRenderer>().GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        var brake = Input.GetKey(KeyCode.Space);
+        if(brake && power) return;
+        _wheelCollider.GetWorldPose(out var pos, out var rot);
+        _wheelTransform.position = pos;
+        _wheelTransform.rotation = rot;
+    }
+
+    private void FixedUpdate()
+    {
+        var brake = Input.GetKey(KeyCode.Space);
+        if (steer)
+        {
+            _wheelCollider.steerAngle = SteerAngle * (invertSteer ? -1 : 1);
+        }
+        if (brake && power)
+        {
+            _wheelCollider.brakeTorque = 500.0f;
+        }
+        else
+        {
+            _wheelCollider.brakeTorque = 0;
+        }
+        if (power)
+        {
+            _wheelCollider.motorTorque = Torque;
+        }
+        
+    }
+}
